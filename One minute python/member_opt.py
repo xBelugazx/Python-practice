d5ee8399f37557e1dddb79d76f85cbbc56d59e11
ningning goat <bskdh98@gmail.com>
2025-09-17 16:07:43 -0500

    Update member_opt.py

--- One minute python/member_opt.py
+++ One minute python/member_opt.py
@@ -2,3 +2,15 @@
 # 어떤 값이 어떤 그룹에 포함돼있는지 확인
 print('c' in 'cat')
 print('m' not in 'pass')
+
+# 문자열 처리
+snack = '꿀꽈배기'
+two = '2개'
+
+juseyo = snack + two
+print(juseyo)
+
+# juseyo = juseyo + '주세요'
+juseyo += '주세요'
+
+print(juseyo)
