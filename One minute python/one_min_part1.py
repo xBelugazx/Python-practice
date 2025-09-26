f16e0d0afa2dfce69e7b44fce3f89e95ff4f8901
I'm a beluga <bskdh98@gmail.com>
2025-09-26 00:31:22 -0500

    Update and rename one_min.py to one_min_part1.py

---
+++ One minute python/one_min_part1.py
@@ -0,0 +1,22 @@
+# 문자 자료형
+print('Hello world')
+print('안녕하세요')
+print('10') # 숫자 자료형 -> '숫자 자료형' = 문자 자료형
+
+# 숫자 자료형
+print(1)
+print(3.14)
+
+# 불리안 자료형
+print(False)
+print(True)
+
+# 변수: 어떤 값을 저장하는 공간
+# 변수선언: 어떤 변수를 만들고 값을 저장하는 행위
+envelope1 = 1000
+envelope2 = 20000
+envelope3 = '화이팅'
+
+print(envelope1)
+print(envelope2)
+print(envelope3)
