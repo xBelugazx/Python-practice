00449c59bd1247534aa3083779a4b30a6e0977ae
I'm a beluga <bskdh98@gmail.com>
2025-12-10 01:19:07 -0600

    Create index.py

---
+++ One minute python/index.py
@@ -0,0 +1,17 @@
+''' 
+인덱스 = 몇 번째
+'''
+lang = 'world'
+
+print(lang[0]) # W
+print(lang[4]) # print(lang[-1])
+
+'''
+슬라이싱
+인덱스 기준, 어디부터 어디까지 잘라줌
+'''
+
+print(lang[1:5])
+print(lang[1:])
+print(lang[:4])
+print(lang[:])
