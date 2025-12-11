238ba9a6c01a5dc8d697e9b9ec7c7bb9f0f9daa4
I'm a beluga <bskdh98@gmail.com>
2025-12-11 00:23:16 -0600

    Create string_processing.py

---
+++ One minute python/string_processing.py
@@ -0,0 +1,29 @@
+snack = '꿀꽈배기'
+two = '2개'
+
+juseyo = snack + two
+
+# 문자열 처리
+# juseyo = juseyo + '주세요'
+juseyo += '주세요'
+
+# 숫자도 가능
+num = 5
+# num = num + 2
+num += 2
+
+num -= 1
+num *= 2
+num /= 4
+
+print(num)
+
+# 문자열 길이 구하기
+print(len(juseyo))
+
+snack = '''꿀꽈배기는
+너무
+맛있어요
+'''
+
+print(snack)
