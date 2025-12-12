4a88872309b30e038256dd070483fe811958a8a7
I'm a beluga <bskdh98@gmail.com>
2025-12-11 22:24:39 -0600

    Add string manipulation methods in method1.py

---
+++ One minute python/method1.py
@@ -0,0 +1,10 @@
+# 메소드 = 기능
+letter = 'how are YOU?'
+
+print(letter.lower()) # 모든 내용 소문자로
+print(letter.upper()) # 모든 내용 대문자로
+print(letter.capitalize()) # 첫글자만 대문자
+print(letter.title()) # 각 단어 첫 글자만 대문자
+print(letter.swapcase()) # 대소문자 뒤바꾸기
+print(letter.split()) # 띄어쓰기 기준으로 나눠서 리스트로 반환
+print(letter.count('how')) # 어떤 글자가 몇 번 쓰였는지?
