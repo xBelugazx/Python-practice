c00f40b8bd75cbd7a43fa9d1b4693eac8ef67e58
I'm a beluga <bskdh98@gmail.com>
2026-01-26 23:19:16 -0600

    Update lecture 1.py

--- jocoding python/lecture 1.py
+++ jocoding python/lecture 1.py
@@ -4,3 +4,18 @@ eng = 75
 math = 55
 
 print((korean + eng + math) / 3)
+
+# calculate average
+korean = 80
+eng = 75
+math = 55
+
+print((korean + eng + math) / 3)
+
+# divide driver license's number
+pin = '881120-1068234'
+yyyymmdd = pin[:6]
+num = pin[7:]
+
+print(yyyymmdd)
+print(num)
